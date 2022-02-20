113a7facf9a7dc064e6dae6943d4dcb4ef263c21
William Ku <william.ku@generalassemb.ly>
2022-02-20 15:35:15 +0800

    try out redeploy after change

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,6 @@ function App() {
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   )
